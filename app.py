4a5688b2f4f5eb69564993abc8952e1a927c4140
jaiye agbonavbare <agbojai15@icloud.com>
2020-11-06 21:05:48 -0500

    'major not listed' implemented

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return flask.render_template("index.html")
-    
+
+@app.route('/NOTA')
+def NOTA():
+    return flask.render_template("NOTA-Email-Submit.html")
+
 app.run(
     port = int(os.getenv('PORT',8080)),
     host = os.getenv('IP', '0.0.0.0'),
---
+++ static/index.js
@@ -0,0 +1,1 @@
+//question hiding functionality here
